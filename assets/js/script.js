f792ecd2c4f44796e9016ba6f4e45e1715522a08
Mike Lane <michaelrlane@protonmail.com>
2021-09-26 13:27:00 -0400

    added rows to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,42 @@
 
-//Pulls current date from server and writes it to DATE paragraph
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+//Pulls current date and time and writes it to DATE paragraph
+var currentDate  = new Date(),
+    currentDay   = currentDate.getDate() < 10 
+                 ? '0' + currentDate.getDate() 
+                 : currentDate.getDate(),
+    currentMonth = currentDate.getMonth() < 9 
+                 ? '0' + (currentDate.getMonth() + 1) 
+                 : (currentDate.getMonth() + 1);
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+
+document.getElementById("date").innerHTML = day + ' - ' + currentMonth + '/' + currentDay + '/' +  currentDate.getFullYear();
+
+
+//
+
+var timeArray = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+var currentTime = moment().format("h A");
 
 
